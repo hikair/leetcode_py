d2f57cd9a66d6225e666d82aa36e2f946c5b2cac
PengQian <351477856@qq.com>
2022-05-06 22:06:54 +0800

    最近的请求次数

---
+++ deque/recentCounter.py
@@ -0,0 +1,15 @@
+# 最近的请求次数
+# https://leetcode-cn.com/problems/number-of-recent-calls/
+from collections import deque
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+        return len(self.q)
