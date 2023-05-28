96942203973ea6f01e78724addfdf0e25e0e5766
zpq <351477856@qq.com>
2023-05-29 07:05:14 +0800

    大样本统计

---
+++ math/middle/sampleStats.py
@@ -0,0 +1,38 @@
+# 1093. 大样本统计
+# https://leetcode.cn/problems/statistics-from-a-large-sample/
+from typing import List
+
+
+class Solution:
+    def sampleStats(self, count: List[int]) -> List[float]:
+        total = sum(count)
+        left, right = (total + 1) // 2, (total + 2) // 2
+        min_v, max_v, median, mode, t, freq = 256, 0, 0, 0, 0, 0
+        sum_v = 0
+        for x, y in enumerate(count):
+            sum_v += x * y
+            if y > freq:
+                freq, mode = y, x
+            if y > 0:
+                if min_v == 256:
+                    min_v = x
+                max_v = x
+            if t < right <= (t + y):
+                median += x
+            if t < left <= (t + y):
+                median += x
+            t += y
+        return [min_v, max_v, sum_v / total, median / 2, mode]
+
+
+s = Solution()
+print(9 // 2)
+print(10 // 2)
+count = [0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+print(s.sampleStats(count))
